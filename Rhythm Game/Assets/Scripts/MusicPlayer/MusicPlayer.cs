4aa564fdacadf3266145a023b48aa3a7f1919891
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2026-06-06 11:06:46 -0700

    Component: Music Player

---
+++ Rhythm Game/Assets/Scripts/MusicPlayer/MusicPlayer.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using RoboRyanTron.Unite2017.Variables;
+
+namespace RhythmGame.Music
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class MusicPlayer : MonoBehaviour
+    {
+        [SerializeField] private FloatVariable musicTimePos;
+
+        private AudioSource audioSource;
+
+        private void Awake() {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        private void Update() {
+            if (audioSource.isPlaying) {
+                musicTimePos.Value = (float)AudioSettings.dspTime;
+            }
+        }
+
+        public void ChangeAndPlayTrack(AudioClip newTrack) {
+            audioSource.Stop();
+            audioSource.clip = newTrack;
+            audioSource.Play();
+        }
+    }
+}
